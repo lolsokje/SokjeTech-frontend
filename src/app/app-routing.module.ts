2ba7eab787d4e9dcdd28dc69b7f1f9afcbf546dc
avandaalen <avandaalen@prophets.be>
2019-04-15 18:22:14 +0200

    Initial work on layouts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyTeamsComponent } from 'src/app/components/my-teams/my-teams.component';
 
 const routes: Routes = [
-  {}
+  { path: 'my-teams', component: MyTeamsComponent }
 ];
 
 @NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { MenuComponent } from './components/menu/menu.component';
+import { MyTeamsComponent } from './components/my-teams/my-teams.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    MyTeamsComponent
   ],
   imports: [
     BrowserModule,
